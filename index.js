9d31cd1b376f98a9e1d906b8102d0ed7dea80e30
anbarasan <mechanbu42@gmail.com>
2024-10-23 16:26:01 +0530

    second

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const root = document.getElementById('root')
+const button = document.getElementById('btn').onclick = function () {
+    alert("Items added to cart successfully");
+}
+const cart = document.getElementById('cart')
+const user = document.getElementById('user')
 
-
-function create(){
-    
+cart.onclick = function () {
+    alert("the items are");
+}
+user.onclick = function () {
+    alert("User details");
 }
